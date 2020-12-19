8b800f1d89fa46dfbabba70f94fe30e8207b7bad
Mikhail Shurshikov <m.shurshikov@gmail.com>
2020-12-19 21:05:00 +0100

    playing with Binding types

--- WebContent/controller/main.controller.js
+++ WebContent/controller/main.controller.js
@@ -5,24 +5,33 @@ sap.ui.define([
 	"use strict";
 	return Controller.extend("controller.main", {
 		onInit: function() {
-			debugger;
-			
+			//JSON Model from File
+			/*			
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.loadData("model/week.json", null, false); //URL, Parameters, Asynchron
-			
 			this.getView().setModel(oModel);
-			
+			*/
+			//Json Model on-the-fly
 			var oData = {
 				value: ""
 			};
 			
 			var oDataModel = new sap.ui.model.json.JSONModel(oData);
+			
+			oDataModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay); //Binding Mode One way --> Change will not be saved in the model
 			this.getView().setModel(oDataModel);
+			
+			var mode = this.getView().getModel().getDefaultBindingMode();
+			if (mode === sap.ui.model.BindingMode.OneWay) {
+				this.getView().byId("BindMode").setText("One-Way");
+			} else {
+				this.getView().byId("BindMode").setText("Two-Way");
+			}
 		},
 		
 		onButtonPressed: function(oEvent) {
 			MessageToast.show("Button pressed");
-			
+
 			//Button Instance
 			oEvent.getSource().setText("Enough");
 			oEvent.getSource().setEnabled(false);
