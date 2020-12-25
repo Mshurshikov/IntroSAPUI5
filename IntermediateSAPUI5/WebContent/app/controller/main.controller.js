b612f89a2868ff9ce73c520a10888116b2aec27e
Mikhail Shurshikov <m.shurshikov@gmail.com>
2020-12-25 19:39:25 +0100

    added custom data type .myPasswordType

--- IntermediateSAPUI5/WebContent/app/controller/main.controller.js
+++ IntermediateSAPUI5/WebContent/app/controller/main.controller.js
@@ -41,6 +41,25 @@ sap.ui.define([
 				jQuery.sap.require("sap.m.MessageToast");
 				sap.m.MessageToast.show("Right input");
 			}
-		}
+		},
+		
+		myPasswordType: sap.ui.model.SimpleType.extend("Password", {
+			formatValue: function(oValue) {
+				return oValue;
+			},
+			
+			parseValue: function(oValue) {
+				return oValue;
+			},
+			
+			validateValue: function(oValue) {
+				console.log(oValue);
+				if(!/^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/.test(oValue)) {
+					throw new sap.ui.model.ValidateException("Password: 8-16 characters and numbers");
+				} else {	
+					return oValue;
+				}
+			}
+		})
 	});	
 });
