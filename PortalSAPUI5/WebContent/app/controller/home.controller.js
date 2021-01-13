017a019a1e3746301b754b550b412fd9fc8b6d02
Mikhail Shurshikov <m.shurshikov@gmail.com>
2021-01-13 15:11:16 +0100

    init home view controller

---
+++ PortalSAPUI5/WebContent/app/controller/home.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/Controller"
+], function(Controller) {
+	"use strict";
+	return Controller.extend("my.app.controller.home", {
+		onInit: function() {
+			
+		}
+	});
+});
