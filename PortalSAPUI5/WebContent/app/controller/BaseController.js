ab49cf17ecfa3427ae7b58bee568ec4e79f42fa5
Mikhail Shurshikov <mikhail.shurshikov@mewa.de>
2021-01-15 13:35:07 +0100

    implement show username in home.view

--- PortalSAPUI5/WebContent/app/controller/BaseController.js
+++ PortalSAPUI5/WebContent/app/controller/BaseController.js
@@ -6,6 +6,21 @@ sap.ui.define([
 	return Controller.extend("my.app.controller.BaseController", {
 		getRouter:function() {
 			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		
+		getModel: function(sName) {
+			var oModel = this.getView().getModel(sName);
+			if(!oModel) {
+				oModel = this.getComponent().getModel(sName);
+			}
+			return oModel;
+		},
+		
+		getComponent: function() {
+			var sComponentId = sap.ui.core.Component.getOwnerIdFor(this.getView());
+			if (sComponentId) {
+				return sap.ui.component(sComponentId);
+			}
 		}
 	});
 });
--- PortalSAPUI5/WebContent/app/controller/root.controller.js
+++ PortalSAPUI5/WebContent/app/controller/root.controller.js
@@ -1,10 +1,39 @@
 sap.ui.define([
-	"my/app/controller/BaseController"
-], function(Controller) {
+	"my/app/controller/BaseController",
+	"sap/m/MessageBox"
+], function(Controller, MessageBox) {
 	"use strict";
 	return Controller.extend("my.app.controller.root", {
 		onInit: function() {
-			
+			/* 
+			 * Username is not known, read first entry from Model	
+			 */
+			this.getModel("portal").read("/UserSet", {
+				success: jQuery.proxy(function(oEvent) {
+					if(!oEvent.results.length) {return;}
+					
+					var sUsername = oEvent.results[0].Username;
+					var oUserHeadItem = this.getView().byId("UserHeadItem");
+					
+					oUserHeadItem.bindProperty("username", {
+						parts: [
+							"portal>/UserSet('" + sUsername + "')/Firstname",
+							"portal>/UserSet('" + sUsername + "')/Lastname"
+						],
+						formatter: function(sFirst, sLast) {
+							sFirst = sFirst || "";
+							sLast = sLast || "";
+							return sFirst + " " + sLast;
+						}
+					});
+				}, this),
+				error: jQuery.proxy(function() {
+					MessageBox.error(
+						this.getText("message.read.user.error"),
+						{ title: this.getText("message.error")}
+					);
+				}, this)
+			});
 		}
 	});
 });
