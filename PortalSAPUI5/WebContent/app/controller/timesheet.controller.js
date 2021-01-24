8708466a86dbeddfcb87c3c6b95fa14b1f52b11f
Mikhail Shurshikov <m.shurshikov@gmail.com>
2021-01-24 18:12:44 +0100

    implemented basic functions

--- PortalSAPUI5/WebContent/app/controller/timesheet.controller.js
+++ PortalSAPUI5/WebContent/app/controller/timesheet.controller.js
@@ -1,14 +1,56 @@
 sap.ui.define([
-	"my/app/controller/BaseController"
-], function(Controller) {
+	"my/app/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 	return Controller.extend("my.app.controller.timesheet", {
 		onInit: function() {
+			this.getModel("portal").attachRequestCompleted(this.onRequestCompleted, this);
+			this._createChartModel();
+			this._configureChart();
+		},
+		
+		onExit: function() {
+			this.getModel("portal").detachRequestCompleted(this.onRequestCompleted, this);
+		},
+		
+		//Event section
+		onBackPressed: function() {
+			this.getRouter().navTo("home");
+		},
+		
+		onNewPressed: function() {
 			
 		},
 		
-		onBackPressed: function() {
-			this.getRouter().navTo("home");
+		onNewCancelled: function() {
+			this._oNewDialog.close();
+		},
+		
+		onNewSaved: function(oEvent) {
+			
+		},
+		
+		onTimeTypeChanged: function(oEvent) {
+			
+		},
+		
+		onProjectChanged: function(oEvent) {
+			
+		},
+		
+		onRequestCompleted: function(oEvent) {
+			
+		},
+		
+		//Private section
+		_createChartModel: function() {
+			this._oChartModel = new JSONModel();
+			this.getView().setModel(this._oChartModel, "chart");
+		},
+		
+		_configureChart: function() {
+			
 		}
 	});
 });
